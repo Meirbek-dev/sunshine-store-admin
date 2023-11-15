538f47098aa03cef64c5219e983eab58791fb733
Meirbek Beisenov <8057115-m.beysenov@users.noreply.gitlab.com>
2023-11-15 10:42:03 +0600

    fixed an image uploading bug

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -73,9 +73,9 @@ export async function POST(req: Request, {params}: { params: { storeId: string; 
 export async function GET(req: Request, {params}: { params: { storeId: string; }; },) {
     try {
         const {searchParams} = new URL(req.url);
-        const categoryId = searchParams.get('categoryId') ?? undefined;
-        const colorId = searchParams.get('colorId') ?? undefined;
-        const sizeId = searchParams.get('sizeId') ?? undefined;
+        const categoryId = searchParams.get('categoryId') || undefined;
+        const colorId = searchParams.get('colorId') || undefined;
+        const sizeId = searchParams.get('sizeId') || undefined;
         const isFeatured = searchParams.get('isFeatured');
 
         if (!params.storeId) {
